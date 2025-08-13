e344af04ea3548f0e3c1543d3e3238b3ca558ce2
Christian Acevedo <christian.acevedo.85@gmail.com>
2025-08-13 12:01:13 -0500

    semiarreglado

--- src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
+++ src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
@@ -3,15 +3,10 @@ package com.kenni.datos.datos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DatosApplicationTests {
-
-        @MockBean
-        private PubSubTemplate pubSubTemplate;
 
 	@Test
 	void contextLoads() {
