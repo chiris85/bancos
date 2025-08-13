64203bd1ef98fe99433e2b95eea25e3a9e180280
Christian Acevedo <christian.acevedo.85@gmail.com>
2025-08-13 12:11:28 -0500

    tercerarreglo

--- src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
+++ src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class DatosApplicationTests {
+public class DatosApplicationTests {
 
 	@Test
 	void contextLoads() {
