0200a22281637902676c00b76039af1ff195c408
Christian Acevedo <christian_acevedo@hotmail.com>
2025-08-13 11:30:27 -0500

    Mock PubSubTemplate bean for tests

--- src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
+++ src/test/java/com/kenni/datos/datos/DatosApplicationTests.java
@@ -3,10 +3,15 @@ package com.kenni.datos.datos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DatosApplicationTests {
+
+        @MockBean
+        private PubSubTemplate pubSubTemplate;
 
 	@Test
 	void contextLoads() {
